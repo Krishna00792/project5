0f867afa925739df9fbdb5c97e9338d208e15600
Krishna00792 <sai017krishna@gmail.com>
2023-05-24 14:39:40 +0530

    projectone

---
+++ src/components/Home.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>Techacker</h1>
+          <p>Solution to all your problems</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children.
+          </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            officiis hic illo quos sit commodi, aut magnam odio eligendi? Totam
+            quis officiis fugit sunt dolores nostrum tenetur iusto est odio
+            mollitia.et? Aliquid nemo nesciunt placeat,
+            qui fuga quae repellat sunt, non natus, iure ab officia rem ipsa cum
+            sed cumque corporis voluptate quibusdam nostrum quod saepe
+          </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Services.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import img1 from "../assets/3.jpg";
+import img2 from "../assets/4.jpg";
+
+const Services = () => {
+  return (
+    <div>
+      <Carousel
+        infiniteLoop
+        autoPlay
+        showStatus={false}
+        showArrows={false}
+        showThumbs={false}
+        interval={2000}
+      >
+        <div style={{height:'380px'}}>
+          <img src={img1} alt="Item1" style={{minHeight:'370px'}} />
+          <p className="legend">Full Stack</p>
+        </div>
+        <div style={{height:'380px'}}>
+          <img src={img2} alt="Item3" style={{minHeight:'370px'}} />
+          <p className="legend">Peer-to-peer Support</p>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Services;
